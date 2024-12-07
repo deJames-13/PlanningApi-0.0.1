30791a5abb275e1c36e26d4973b14f61d9fe6536
deJames-13 <ail>
2024-12-08 02:11:02 +0800

    gh

--- app/Http/Resources/ParticularResource.php
+++ app/Http/Resources/ParticularResource.php
@@ -14,6 +14,9 @@ class ParticularResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            ...parent::toArray($request),
+            'values' => $this->values,
+        ];
     }
 }
