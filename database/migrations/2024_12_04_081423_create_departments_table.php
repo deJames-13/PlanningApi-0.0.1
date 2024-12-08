562168f2512f8aade324fd7d5c844f0a903dbc68
deJames-13 <ail>
2024-12-08 19:08:11 +0800

    ads

--- database/migrations/2024_12_04_081423_create_departments_table.php
+++ database/migrations/2024_12_04_081423_create_departments_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('short_name')->nullable();
             $table->string('full_name')->nullable();
-            
+            $table->enum('type', ['division', 'department'])->default('division');
             
             $table->softDeletes();
             $table->timestamps();
