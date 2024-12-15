bf7d1990f6190e647229ed4ae3e8f2a3d8f3a728
deJames-13 <ail>
2024-12-15 16:36:37 +0800

    asdf

--- config/cors.php
+++ config/cors.php
@@ -4,10 +4,11 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'api/login', 'api/logout', 'api/refresh'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', '*'), 'http://localhost:3000'],
+    // 'allowed_origins' => [env('FRONTEND_URL', '*'), 'http://localhost:3000'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'exposed_headers' => ['*'],
+    'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
 ];
